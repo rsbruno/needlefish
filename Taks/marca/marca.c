5e43f9e4c39e00aff79ed3e5585f6f980884a456
rsbruno <bruno.roberto@sou.unifal-mg.edu.br>
2021-07-26 15:55:32 -0300

    feat(task/marca)This find the watermark using subtraction

--- Taks/marca/marca.c
+++ Taks/marca/marca.c
@@ -8,13 +8,16 @@
 #include <stdlib.h>
 #include "imagelib.h"
 
-void waterMark(image In, image Out, int nl, int nc, int mn)
+void waterMark(image In, image Out, int nl, int nc, int mn, image ORG)
 {
     int i, j;
     for (i = 0; i < nl; i++)
         for (j = 0; j < nc; j++)
         {
-            Out[i * nc + j] = mn - In[i * nc + j];
+            if (In[i * nc + j] == ORG[i * nc + j])
+                Out[i * nc + j] = 255;
+            else
+                Out[i * nc + j] = 0;
         }
 }
 
@@ -32,27 +35,29 @@ void msg(char *s)
  *-------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
-    int OK, nc, nr, ml;
+    int OK, nc, nr, ml,
+        nc_org, nr_org, ml_org;
     char name[100];
     char command[110];
-    image In, Out;
+    image In, Out, Original;
     if (argc < 2)
         msg(argv[0]);
 
     In = img_readpgm(argv[1], &nr, &nc, &ml);
+    Original = img_readpgm("stanford.pgm", &nc_org, &nr_org, &ml_org);
+
     printf("\nWaterMark");
     img_info(argv[1], nr, nc, ml);
     Out = img_alloc(nr, nc);
 
     /*-- transformation --*/
-    waterMark(In, Out, nr, nc, ml);
+    waterMark(In, Out, nr, nc, ml, Original);
 
-    sprintf(name, "%s-%s", argv[1], "WaterMark.pgm");
+    sprintf(name, "%s-%s", argv[1], "saida.pgm");
     img_writepgm(Out, name, nr, nc, ml);
     sprintf(command, "%s %s &", VIEW, name);
     system(command);
-    
-    
+
     img_free(In);
     img_free(Out);
     return 0;
